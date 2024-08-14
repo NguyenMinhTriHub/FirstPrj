c5f7de078cc642131b83ae705562079d51635446
NguyenMinhTriHub <mtri20051002@gmail.com>
2024-08-14 20:33:01 +0700

    First Comit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Minh Tri");
+        System.out.println("Welcome to the DHHTTT19A");
+    }
+}
